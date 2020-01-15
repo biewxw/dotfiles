67d95d243dd450ea045e61934ceec6d71af92e41
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-01-15 18:01:10 -0300

    new: first polybar setup and pywal test

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch polybar
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar mybar >>/tmp/polybar1.log 2>&1 &
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar bar1 &
+echo "Bar launched..."
