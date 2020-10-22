e72b1bc6c3d1a0f6284c1ea90f77eb2eb078402a
Gabriel <gabrieldeoliveira.gdo23@gmail.com>
2020-10-22 00:26:03 -0300

    (feat) some bash commands

--- install.sh
+++ install.sh
@@ -1,37 +1,70 @@
-#!/usr/bin/env bash
+# Updating Packages
 
-echo "> Installing: Essential Packages"
+echo "Updating...\n"
 
-sudo pacman -S base-devel
-sudo pacman -S yay
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get upgrade
 
-echo "> Installing: Git Configs"
+# Installing Essentials via Package
 
-git config --global user.name "Gabriel"
-git config --global user.email "gabrieldeoliveira.gdo23@gmail.com"
+echo "\nInstalling Essentials...\n"
 
-echo "> Installing: Essential Apps"
+sudo apt-get install build-essential libssl-dev
 
-yay -Sy nodejs npm
-yay -Sy yarn
-yay -Sy ttf-fira-code
-yay -Sy sonic-pi
-yay -Sy notion-app
-yay -Sy spotify
-yay -Sy discord
-yay -Sy firefox-developer-edition
-yay -Sy visual-studio-code-insiders
-yay -Sy telegram-desktop
-yay -Sy insomnia
-yay -Sy obsidian-appimage
-yay -Sy wps-office
-yay -Sy docker
-yay -Sy vue-cli
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-echo "> Installing: Oh My ZSH and Terminal Packages"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn
 
-yay -Sy screenfetch
-yay -Sy neofetch
-yay -Sy htop
-yay -Sy zsh
-curl -L http://install.ohmyz.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/v0.25.0/install.s | bash
+
+sudo apt-get install zsh
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+sudo apt-get install docker
+sudo apt-get install docker-compose
+sudo apt-get install neovim
+sudo apt-get install neofetch
+sudo apt-get install htop
+sudo apt-get install gnome-tweaks
+sudo apt-get install gnome-tweak-tool
+sudo apt-get install telegram-desktop
+sudo apt-get install snapd
+sudo apt-get install fonts-firacode
+sudo snap install code --classic
+sudo snap install gotop
+sudo snap install insomnia
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install android-studio --classic
+sudo snap install spotify
+
+# Downloading Outside Package
+
+wget https://github.com/vercel/hyper/releases/download/3.0.2/hyper_3.0.2_amd64.deb
+sudo apt install ./hyper_3.0.2_amd64.deb
+
+wget https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-dev/microsoft-edge-dev_88.0.673.0-1_amd64.deb
+sudo apt install ./microsoft-edge-dev_88.0.673.0-1_amd64.deb
+
+# Some Configs
+
+echo "\nGenerating Configurations...\n"
+
+mkdir ~/Documentos/Limbo
+mkdir ~/Documentos/Work
+mkdir ~/Documentos/Extensions
+mkdir ~/Documentos/Course
+mkdir ~/Documentos/Contrib
+git config --global user.name 'Gabriel'
+git config --global user.email 'gabrieldeoliveira.gdo23@gmail.com'
+cp .oh-my-zsh ~/
+cp .zshrc ~/
+cp .icons ~/
+cp .themes ~/
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /opt/Hyper/hyper 50
+# ssh-keygen -t rsa -b 4096 -C "gabrieldeoliveira.gdo23@gmail.com"
+git clone https://github.com/biewxw/dotfiles.git ~/Documentos/Limbo/dotfiles
+git clone https://github.com/biewxw/garden.git ~/Documentos/Limbo/garden
